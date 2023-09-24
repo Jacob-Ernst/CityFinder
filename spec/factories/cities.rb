2bee27e077c653d7af0ee318b3b6ea3509253b47
Jacob Ernst <akumadude.je@gmail.com>
2023-09-24 14:28:01 -0500

    Updating DB config and adding tests for city reverse geocoding

--- spec/factories/cities.rb
+++ spec/factories/cities.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :city do
-    
+    name { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    lat { Faker::Address.latitude }
+    lng { Faker::Address.longitude }
+    population { Faker::Number.number(digits: 7) }
   end
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,51 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe City do
+  describe 'after_validation' do
+    context 'when creating new city and coordinates present' do
+      subject(:city) do
+        build(:city, lat: 30.267153, lng: -97.7430608)
+      end
+
+      it 'reverse geocodes the city' do
+        city.save
+        expect(city.address).to eq('Austin, TX, USA')
+      end
+    end
+
+    context 'when creating new city and coordinates missing' do
+      subject(:city) { build(:city, lat: nil, lng: nil) }
+
+      it 'does not reverse geocode the city' do
+        city.save
+        expect(city.address).to be_nil
+      end
+    end
+
+    context 'when updating city and coordinates unchanged' do
+      subject(:city) { create(:city, lat: nil, lng: nil) }
+
+      it 'does not reverse geocode the city' do
+        expect do
+          city.name = 'Doge City'
+          city.save
+        end.not_to change(city, :address)
+      end
+    end
+
+    context 'when updating city and coordinates changed' do
+      subject(:city) { create(:city) }
+
+      it 'reverse geocodes the city' do
+        expect do
+          city.lat = 30.267153
+          city.lng = -97.7430608
+          city.save
+        end.to change(city, :address)
+        expect(city.address).to eq('Austin, TX, USA')
+      end
+    end
+  end
 end
