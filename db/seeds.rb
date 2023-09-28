f9023738b85b932af1fd06ed70751cc7354fcbec
Jacob Ernst <akumadude.je@gmail.com>
2023-09-28 07:15:26 -0500

    Creating basic seeder for City data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cities_import_path = Rails.root.join('db/data/cities.csv')
+
+Rails.logger.info "Importing cities from #{cities_import_path}.\n"
+
+# Use transaction to rollback all changes in case we run into errors.
+ActiveRecord::Base.transaction do
+  # Use block form of CSV.foreach because it closes the file automatically.
+  # Convert headers to symbols to simplify assigning properties.
+  CSV.foreach(
+    cities_import_path, headers: true, header_converters: :symbol
+  ) do |row|
+    city = City.find_or_initialize_by(name: row[:name], state: row[:state])
+    city.assign_attributes(row.to_h)
+    city.save!(validate: false)
+  end
+end
+
+Rails.logger.info "\nDone importing cities!"
